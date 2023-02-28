ac1475c8d883a4e1fef02d40a9730fd29c7dc99c
viniciusClunc <vinicius@valorizza.com.br>
2023-02-28 09:06:59 -0300

    Intro concluida

--- src/pages/Home/components/Intro/index.tsx
+++ src/pages/Home/components/Intro/index.tsx
@@ -1,11 +1,56 @@
-import { IntroContainer, IntroContent } from "./styles";
+import {
+  BenefitContainer,
+  IntroContainer,
+  IntroContent,
+  IntroTitle,
+} from "./styles";
+import introImg from "../../../../assets/intro-imagem.png";
+import { RegularText } from "../../../../components/Typography";
+import { InfoWithIcon } from "../../../../components/InfoWithIcon";
+import { ShoppingCart, Package, Timer, Coffee } from "phosphor-react";
+import { useTheme } from "styled-components";
 
 export function Intro() {
-    return (
-        <IntroContainer>
-            <IntroContent className="container">
-                <p>teste</p>
-            </IntroContent>
-        </IntroContainer>
-    )
+  const { colors } = useTheme();
+
+  return (
+    <IntroContainer>
+      <IntroContent className="container">
+        <div>
+          <section>
+            <IntroTitle size="xl">
+              Encontre o café perfeito para qualquer hora do dia
+            </IntroTitle>
+            <RegularText size="l" color="subtitle" as="h3">
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </RegularText>
+          </section>
+          <BenefitContainer>
+            <InfoWithIcon
+              iconBg={colors["brand-yellow-dark"]}
+              icon={<ShoppingCart weight="fill" />}
+              text="Compra simples e segura"
+            />
+            <InfoWithIcon
+              iconBg={colors["base-text"]}
+              icon={<Package weight="fill" />}
+              text="Embalagem mantém o café intacto"
+            />
+            <InfoWithIcon
+              iconBg={colors["brand-yellow"]}
+              icon={<Timer weight="fill" />}
+              text="Entrefa rápida e rastreada"
+            />
+            <InfoWithIcon
+              iconBg={colors["brand-purple"]}
+              icon={<Coffee weight="fill" />}
+              text="O café chega fresquinho até você"
+            />
+          </BenefitContainer>
+        </div>
+        <img src={introImg} alt="Imagem de café e copo plástico" />
+      </IntroContent>
+    </IntroContainer>
+  );
 }
