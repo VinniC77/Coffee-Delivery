8dd724e93037f2fd67031a94756c8628e61743af
viniciusClunc <vinicius@valorizza.com.br>
2023-03-02 18:26:17 -0300

    Inicio do Card

---
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -0,0 +1,9 @@
+import { CoffeeCardContainer } from "./styles";
+
+export default function CoffeeCard() {
+    return (
+        <CoffeeCardContainer>
+            <img src="https://s3-alpha-sig.figma.com/img/55b1/f9ee/64600f98b2bae456b96fdc624c4b4f47?Expires=1678665600&Signature=k8VKVHILYiqREu5q~iln7daqRJJPd-Xv8~Bm~IwbRxO0RoALjNudlhvYoHcpEV8bToHcuQQ5TgleURrlvEjMdGuadmx~STRTpl65dfO21LxfAR9UYt7DXdEswQpp~1-cKuo2RbKdBmy9dCGuvoP3tkjVIcdOGQTeZHhjZoMh2uKKwri2C6PjNJT-coQnRvaw9C6EUU0xFxRZ0qc4yUHe5sEglW7nkONh4iAcGTF9Aon4xkPehZRjiSS3FA6dmloLCWeIBFa2n92pKWSpI2FvGMBpsctApXO0X8cu0muV-UoiW8CDYzQ2dVsR6m79PtGlrwKCcOePbJSTXZmmlJZf2g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="Xícara com café" />
+        </CoffeeCardContainer>
+    )
+}
---
+++ src/pages/Home/components/OurCoffees/index.tsx
@@ -0,0 +1,19 @@
+import { TitleText } from "../../../../components/Typography";
+import CoffeeCard from "../CoffeeCard";
+import { CoffeeList, OurCoffeesContainer } from "./styles";
+
+export default function OurCoffees() {
+    return (
+        <OurCoffeesContainer className="container">
+            <TitleText size="l" color="subtitle">
+                Nossos cafés
+            </TitleText>
+            <CoffeeList>
+            <CoffeeCard />
+            <CoffeeCard />
+            <CoffeeCard />
+            <CoffeeCard />
+            </CoffeeList>
+        </OurCoffeesContainer>
+    )
+}
