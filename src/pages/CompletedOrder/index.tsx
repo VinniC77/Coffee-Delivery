46c9ba6a39fce3e255b3234f9e9ac321c567c615
viniciusClunc <vinicius@valorizza.com.br>
2023-03-08 13:56:40 -0300

    Finalização do Form

--- src/pages/CompletedOrder/index.tsx
+++ src/pages/CompletedOrder/index.tsx
@@ -1,36 +1,44 @@
 import CompletedOrderForm from "./components/CompletedOrderForm";
 import SelectedCoffees from "./components/SelectedCoffees";
 import { CompleteOrderContainer } from "./styles";
-import * as zod from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, FormProvider } from 'react-hook-form';
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, FormProvider } from "react-hook-form";
 
 const confirmOrderFormValidationSchema = zod.object({
-  cep: zod.string()
-})
+  cep: zod.string().min(1, "Informe o CEP"),
+  street: zod.string().min(1, "Informe a Rua"),
+  number: zod.string().min(1, "Informe o Número"),
+  complment: zod.string(),
+  district: zod.string().min(1, "Informe o Bairro"),
+  city: zod.string().min(1, "Informe a Cidade"),
+  uf: zod.string().min(1, "Informe o Estado"),
+});
 
 export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>;
 
 type ConfirmOrderFormData = OrderData;
 
 export function CompletedOrderPage() {
-
   const confirmOrderForm = useForm<ConfirmOrderFormData>({
-    resolver: zodResolver(confirmOrderFormValidationSchema)
-  })
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  });
 
   const { handleSubmit } = confirmOrderForm;
 
   function handleConfirmOrder(data: ConfirmOrderFormData) {
-    console.log(data)
+    console.log(data);
   }
 
   return (
     <FormProvider {...confirmOrderForm}>
-    <CompleteOrderContainer className="container" onSubmit={handleSubmit(handleConfirmOrder)}>
-      <CompletedOrderForm />
-      <SelectedCoffees />
-    </CompleteOrderContainer>
+      <CompleteOrderContainer
+        className="container"
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
+        <CompletedOrderForm />
+        <SelectedCoffees />
+      </CompleteOrderContainer>
     </FormProvider>
   );
 }
